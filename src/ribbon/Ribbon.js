9fa518876dd58fb21443f9c836e6543a1194da2c
Nick <zudo@doki.space>
2021-05-29 16:38:19 +0100

    Update Ribbon.js

--- src/ribbon/Ribbon.js
+++ src/ribbon/Ribbon.js
@@ -4,7 +4,7 @@ const EventEmitter = require("events");
 const Room = require("./Room");
 const api = require("../gameapi/api");
 
-const CLIENT_VERSION = {"id": "8af1892", "time": 1621297421000};
+const CLIENT_VERSION = {"id":"f273787","time":1622209405000};
 
 const RIBBON_ENDPOINT = "wss://tetr.io/ribbon";
 

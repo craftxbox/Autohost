1e6063b422b85b858e82dd4b37ff4abc265f20c8
craftxbox <craftxbox@craftxbox.com>
2022-07-20 18:20:57 -0230

    log colors qol

--- src/log.js
+++ src/log.js
@@ -1,4 +1,6 @@
 const {DBLog} = require("./db/models");
+const chalk = require("chalk");
+
 
 const LOG_LEVELS = {
     ULTRAFINE: 10,
@@ -9,10 +11,19 @@ const LOG_LEVELS = {
     CRITICAL: 60
 };
 
+const _LEVEL_COLORS = {
+    [LOG_LEVELS.ULTRAFINE]: chalk.cyan,
+    [LOG_LEVELS.FINE]: chalk.green,
+    [LOG_LEVELS.INFO]: chalk.white,
+    [LOG_LEVELS.WARNING]: chalk.yellow,
+    [LOG_LEVELS.ERROR]: chalk.red,
+    [LOG_LEVELS.CRITICAL]: chalk.redBright.bold
+};
+
 function logMessage(level, component, message, meta) {
     const time = new Date();
-    const wn = global?.workerName || "primary";
-    console.log(`{${wn}} [${component}] [${time.toLocaleString()}] ${message}`);
+    const wn = global?.workerName || chalk.redBright("?unkn?");
+    console.log(`[${wn}] [${_LEVEL_COLORS[level](component)}] [${_LEVEL_COLORS[level](time.toLocaleString())}] ${_LEVEL_COLORS[level](message)}`);
     DBLog.create({
         level, time, component, message, meta,
         worker: wn
